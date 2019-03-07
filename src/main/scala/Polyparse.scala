21b6f595a23a54aaf60c514cdd2a40b9360deab9
fhackett <fhackett.py@gmail.com>
2019-03-07 04:29:21 -0500

    Add argument types; bare minimum WIP

--- src/main/scala/Polyparse.scala
+++ src/main/scala/Polyparse.scala
@@ -4,54 +4,25 @@ import scala.quoted._
 //import scala.quoted.Toolbox.Default._
 
 
-/*sealed trait A[T:Type] {
-  val t = implicitly[Type[T]]
-}
-
-case class B[T:Liftable:Type](val v : T) extends A[T] {
-  val liftable = implicitly[Liftable[T]]
-}
-case class C[T1:Type,T2:Type](val v1 : A[T1], val v2 : A[T2]) extends A[(T1,T2)]
-
-object D {
-  def proc[T:Type](a : A[T]) : Expr[T] = a match {
-    case b@B(v) => v.toExpr(b.liftable)
-    case C(v1,v2) => {
-      implicit val v1t = v1.t
-      implicit val v2t = v2.t
-      '( (~proc(v1), ~proc(v2)) )
-    }
-  }
-} */
-
-sealed trait Grammar[T:Type,ET:Type] {
+sealed trait Grammar[AT:Type,T:Type,ET:Type] {
   val tType = implicitly[Type[T]]
   val eType = implicitly[Type[ET]]
+  val aType = implicitly[Type[AT]]
 }
 
-sealed trait Value[T] {
-  def ++[T2](v2 : Value[T2]) : Value[(T,T2)] = PairValue(this, v2)
-  def map[T2](fn : Expr[T] => Expr[T2]) : Value[T2] = MappedValue(this, fn)
-}
-
-case class PlainValue[T : Liftable](val v : T) extends Value[T] {
+private case class Terminal[T:Type:Liftable](val v : T) extends Grammar[Unit,T,T] {
   val liftable = implicitly[Liftable[T]]
 }
-case class MappedValue[T1,T2](val v : Value[T1], val fn : Expr[T1] => Expr[T2]) extends Value[T2]
-case class PairValue[T1,T2](val v1 : Value[T1], val v2 : Value[T2]) extends Value[(T1,T2)]
-class RegisterValue[T] extends Value[T]
-
-private case class Terminal[T:Type](val v : Value[T]) extends Grammar[T,T]
-private case class Tuple[A:Type,B:Type,ET:Type](val left : Grammar[A,ET], val right : Grammar[B,ET]) extends Grammar[(A,B),ET]
-//private case class IgnoreLeft[T](val left : Grammar[T], val right : Grammar[T]) extends Grammar[T]
-//private case class IgnoreRight[T](val left : Grammar[T], val right : Grammar[T]) extends Grammar[T]
-private case class Disjunction[T:Type,ET:Type](val left : Grammar[T,ET], val right : Grammar[T,ET]) extends Grammar[T,ET]
-private case class Condition[T:Type,ET:Type](val g : Grammar[T,ET], val cond : Expr[T] => Expr[Boolean]) extends Grammar[T,ET]
-private case class PutValue[T:Type,ET:Type](val v : Value[T]) extends Grammar[T,ET]
-private case class TakeValue[T1,T2:Type,ET:Type](val g : Grammar[T1,ET], val v : RegisterValue[T1], val gg : Grammar[T2,ET]) extends Grammar[T2,ET]
-private case class Mapping[T1,T2:Type,ET:Type](val from : Grammar[T1,ET], val m : Expr[T1] => Expr[T2]) extends Grammar[T2,ET]
-//private case class AppliedLam[A,T](val reg : RegisterValue[A], val arg : Value[A], val body : Grammar[T]) extends Grammar[T]
-private class Recursion[T:Type,ET:Type](_g : =>Grammar[T,ET]) extends Grammar[T,ET] {
+private case class ArgumentTerminal[T:Type]() extends Grammar[T,T,T]
+private case class ArgToVal[T:Type]() extends Grammar[T,T,Any]
+private case class Call[AT1:Type,AT2:Type,T:Type,ET:Type](val arg : Grammar[AT1,AT2,ET], val fn : Grammar[AT2,T,ET]) extends Grammar[AT1,T,ET]
+private case class Sequence[LAT:Type,RAT:Type,LT:Type,RT:Type,ET:Type](val left : Grammar[LAT,LT,ET], val right : Grammar[RAT,RT,ET]) extends Grammar[(LAT,RAT),(LT,RT),ET]
+private case class Check[AT:Type,ET:Type](val g : Grammar[AT,Any,ET]) extends Grammar[AT,Unit,ET]
+private case class Disjunction[AT:Type,T:Type,ET:Type](val left : Grammar[AT,T,ET], val right : Grammar[AT,T,ET]) extends Grammar[AT,T,ET]
+private case class Condition[AT:Type,T:Type,ET:Type](val g : Grammar[AT,T,ET], val cond : Expr[T] => Expr[Boolean]) extends Grammar[AT,T,ET]
+private case class MapArg[AT1:Type,AT2,T:Type,ET:Type](val to : Grammar[AT2,T,ET], val fn : Expr[AT1] => Expr[AT2]) extends Grammar[AT1,T,ET]
+private case class MapVal[AT:Type,T1,T2:Type,ET:Type](val from : Grammar[AT,T1,ET], val fn : Expr[T1] => Expr[T2]) extends Grammar[AT,T2,ET]
+private class Recursion[AT:Type,T:Type,ET:Type](_g : =>Grammar[AT,T,ET]) extends Grammar[AT,T,ET] {
   lazy val g = _g
 }
 
@@ -64,21 +35,35 @@ trait SequenceContext[ET:Type] {
   def next() : Expr[Unit]
 }
 
-/*class Block[Input,Result](val e : Expr[Unit], val pc : Int)(implicit ctx : ControlFlowContext) {
-  var result : Expr[Result]
+class BlockContext {
+  import collection.mutable.Map
+  val blocks : Map[Int,Block[_]] = Map.empty
+  var nextPC = 0
+  def makeBlock[Input](fn : Expr[Input] => Expr[Any]) : Block[Input] = {
+    val nBlock = Block(nextPC, fn)
+    blocks(nextPC) = nBlock
+    nextPC += 1
+    nBlock
+  }
+  /*def reify[Result:Type](initBlock : Block[_,_]) : Expr[Result] = '{
+    var pc = ~block.label
+    var arg : Any = Null
+    var loop = true
+    while(loop) {
+      ~{
+        (0 until nextPC).foldRight('())((i, r) => '{ if pc == ~(i) then {
+          arg = ~blocks(i).fn('(arg.asInstanceOf[~[blocks(i).aType]]))
+        } else ~r })
+      }
+    }
+  }*/
 }
 
-class ControlFlowContext {
-  var nextPC = 0
-  var blockMap : Map[Int,Block] = Map.empty
-  def addBlock = {
-    val pc = nextPC
-    nextPC += 1
-    val block = new Block('(), pc, this)
-    blockMap = blockMap + (pc, block)
-    block
+case class Block[Input](val label : Int, val fn : Expr[Input] => Expr[Any]) {
+  def goto(input : Expr[Input])(implicit ctx : BlockContext) : Expr[Unit] = {
+    '()
   }
-}*/
+}
 
 trait MakeSequenceContext[Seq[_]] {
   def makeCtx[ET:Type,Result:Type](fn : SequenceContext[ET] => Expr[Result]) : Expr[Seq[ET] => Result]
@@ -86,48 +71,18 @@ trait MakeSequenceContext[Seq[_]] {
 
 object PolyParse {
 
-  /*sealed abstract class GLam[-Arg, SR <: GLam[_,SR]] {
-    type SResult = SR
-    def sub[A2](reg2 : RegisterValue[A2], arg : Value[A2]) : SR
+  private def maybeRecurse[AT:Type,T:Type,ET:Type](g : =>Grammar[AT,T,ET]) : Grammar[AT,T,ET] = if g == null then new Recursion(g) else g
+
+  implicit class MaybeRecursion[AT1:Type,T:Type,ET:Type](g : =>Grammar[AT1,T,ET]) {
+    def <|>(other : =>Grammar[AT1,T,ET]) : Grammar[AT1,T,ET] = Disjunction(maybeRecurse(g), maybeRecurse(other))
+    def ++[AT2:Type,Other:Type](other : =>Grammar[AT2,Other,ET]) : Grammar[(AT1,AT2),(T,Other),ET] = Sequence(maybeRecurse(g), maybeRecurse(other))
+    //def map[T2:Type](fn : Expr[T] => Expr[T2]) : Grammar[T2,ET] = Mapping(maybeRecurse(g), fn)
+    //def ??(cond : Expr[T] => Expr[Boolean]) : Grammar[T,ET] = Condition(maybeRecurse(g), cond)
   }
 
-  case class Lam[A,T](val reg : RegisterValue[A], val g : Grammar[T]) extends GLam[A,Lam[A,T]] {
-    def apply(arg : Value[A]) : Grammar[T] = AppliedLam(reg, arg, g)
-    def sub[A2](reg2 : RegisterValue[A2], arg : Value[A2]) : Lam[A,T] = Lam(reg,AppliedLam(reg2, arg, g))
-  }
-  case class PLam[A,L<:GLam[_,L]](val reg : RegisterValue[A], val l : L) extends GLam[A,PLam[A,L]] {
-    def apply(arg : Value[A]) : l.SResult = l.sub(reg, arg)
-    def sub[A2](reg2 : RegisterValue[A2], arg : Value[A2]) : PLam[A,L] = PLam[A,L](reg, l.sub(reg2, arg))
-  }*/
-
-  private def maybeRecurse[T:Type,ET:Type](g : =>Grammar[T,ET]) : Grammar[T,ET] = if g == null then new Recursion(g) else g
-
-  implicit class MaybeRecursion[T:Type,ET:Type](g : =>Grammar[T,ET]) {
-    def <|>(other : =>Grammar[T,ET]) : Grammar[T,ET] = Disjunction(maybeRecurse(g), maybeRecurse(other))
-    def ++[Other:Type](other : =>Grammar[Other,ET]) : Grammar[(T,Other),ET] = Tuple(maybeRecurse(g), maybeRecurse(other))
-    def map[T2:Type](fn : Expr[T] => Expr[T2]) : Grammar[T2,ET] = Mapping(maybeRecurse(g), fn)
-    def ??(cond : Expr[T] => Expr[Boolean]) : Grammar[T,ET] = Condition(maybeRecurse(g), cond)
-    def $$[T2:Type](fn : Value[T] => Grammar[T2,ET]) : Grammar[T2,ET] = {
-      val reg = new RegisterValue[T]()
-      TakeValue(maybeRecurse(g), reg, fn(reg))
-    }
-  }
-
-  def const[T:Liftable:Type](v : T) : Value[T] = PlainValue(v)
-  
-  def value[T:Type,ET:Type](v : Value[T]) : Grammar[T,ET] = PutValue(v)
-
-  def term[T:Type](v : Value[T]) : Grammar[T,T] = Terminal(v)
-
-  /*def lam[A,T](scope : Value[A] => Grammar[T]) : Lam[A,T] = {
-    val reg = new RegisterValue[A]()
-    Lam(reg, scope(reg))
-  }
-
-  def lam[A,L <: GLam[_,L]](scope : Value[A] => L) : PLam[A,L] = {
-    val reg = new RegisterValue[A]()
-    PLam(reg, scope(reg))
-  }*/
+  def term[T:Type:Liftable](v : T) : Grammar[Unit,T,T] = Terminal(v)
+  def $[T:Type] : Grammar[T,T,T] = ArgumentTerminal()
+  def $$[T:Type] : Grammar[T,T,Any] = ArgToVal()
 
   implicit object MakeListContext extends MakeSequenceContext[List] {
     def makeCtx[ET:Type,Result:Type](fn : SequenceContext[ET] => Expr[Result]) : Expr[List[ET] => Result] = '((list_ : List[ET]) => {
@@ -143,22 +98,26 @@ object PolyParse {
     })
   }
 
-  def compile[T:Type,ET:Type,Seq<:[ET]=>Any:MakeSequenceContext](g : Grammar[T,ET])(implicit seqT : Type[Seq[T]]) : Expr[Seq[ET] => Option[T]] = {
-    def findRecursions[T,ET](g : Grammar[T,ET]) : Set[Grammar[_,_]] = g match {
+  /*def makeControlFlow(fn : Block[Unit,Unit] => Unit) : Expr[Unit] = {
+    
+  }*/
+
+  def compile[T:Type,ET:Type,Seq[_]:MakeSequenceContext](g : Grammar[Unit,T,ET])(implicit seqT : Type[Seq[T]]) : Expr[Seq[ET] => Option[T]] = {
+    def findRecursions[AT,T,ET](g : Grammar[AT,T,ET]) : Set[Grammar[_,_,_]] = g match {
       case Terminal(_) => Set.empty
-      case Tuple(a,b) => findRecursions(a) | findRecursions(b)
+      case Sequence(a,b) => findRecursions(a) | findRecursions(b)
       case Disjunction(a,b) => findRecursions(a) | findRecursions(b)
-      case Condition(a,_) => findRecursions(a)
-      case PutValue(_) => Set.empty
-      case TakeValue(a,_,b) => findRecursions(a) | findRecursions(b)
-      case Mapping(a,_) => findRecursions(a)
-      case r : Recursion[T,ET] => Set(r.g)
+      //case Condition(a,_) => findRecursions(a)
+      //case PutValue(_) => Set.empty
+      //case TakeValue(a,_,b) => findRecursions(a) | findRecursions(b)
+      //case Mapping(a,_) => findRecursions(a)
+      case r : Recursion[AT,T,ET] => Set(r.g)
     }
     val recursions = findRecursions(g)
     println(recursions)
 
     implicitly[MakeSequenceContext[Seq]].makeCtx(ctx => {
-      def handleRec[T:Type](g : Grammar[T,ET], recMap : Map[AnyRef,Expr[Option[Any]]]) : Expr[Option[T]] = {
+      def handleRec[AT:Type,T:Type](g : Grammar[AT,T,ET], recMap : Map[AnyRef,Expr[Option[Any]]]) : Expr[Option[T]] = {
         if recursions.contains(g) then '{
           def rec() : Option[T] = ~codegen(g, recMap.+((g, '(rec()))))
           rec()
@@ -166,22 +125,24 @@ object PolyParse {
           ~codegen(g, recMap)
         }
       }
-      def valueCodegen[T:Type](v : Value[T]) : Expr[T] = v match {
-        case p@PlainValue(v) => v.toExpr(p.liftable)
-      }
-      def codegen[T:Type](g : Grammar[T,ET], recMap : Map[AnyRef,Expr[Option[Any]]]) : Expr[Option[T]] = g match {
-        case Terminal(v) => '{
-          if !(~ctx.isEOF()) then {
-            val p = ~ctx.peek()
-            if p == ~valueCodegen(v) then {
-              ~ctx.next()
-              Some(p)
+      def codegen[AT:Type,T:Type](g : Grammar[AT,T,ET], recMap : Map[AnyRef,Expr[Option[Any]]]) : Expr[Option[T]] = g match {
+        case t@Terminal(v) => {
+          implicit val liftable = t.liftable
+          '{
+            if !(~ctx.isEOF()) then {
+              val p = ~ctx.peek()
+              if p == ~v.toExpr then {
+                ~ctx.next()
+                Some(p)
+              } else None
             } else None
-          } else None
+          }
         }
-        case Tuple(a,b) => {
+        case Sequence(a,b) => {
           implicit val at = a.tType
+          implicit val aat = a.aType
           implicit val bt = b.tType
+          implicit val abt = b.aType
           '{ 
             (~handleRec(a, recMap)).flatMap(v1 => (~handleRec(b, recMap)).map(v2 => (v1, v2)))
           }
@@ -189,18 +150,18 @@ object PolyParse {
         case Disjunction(a,b) => '{
           (~handleRec(a, recMap)).orElse(~handleRec(b, recMap))
         }
-        case Condition(a,cond) => '{
+        /*case Condition(a,cond) => '{
           (~handleRec(a, recMap)).flatMap(v => if ~cond('(v)) then Some(v) else None)
-        }
-        case PutValue(v) => '{ None } // FIXME 
-        case TakeValue(a,v,b) => '{ None } // FIXME 
-        case Mapping(a,fn) => {
+        }*/
+        //case PutValue(v) => '{ None } // FIXME 
+        //case TakeValue(a,v,b) => '{ None } // FIXME 
+        /*case Mapping(a,fn) => {
           implicit val at = a.tType
           '{
             (~handleRec(a, recMap)).map(v => ~fn('(v)))
           }
-        }
-        case r : Recursion[T,ET] => '{
+        }*/
+        case r : Recursion[AT,T,ET] => '{
           (~recMap.get(r.g).get.asInstanceOf[Expr[Option[T]]])
         }
       }
@@ -211,7 +172,8 @@ object PolyParse {
   def runTest = {
     import scala.quoted.Toolbox.Default._
     object vv {
-      val v : Grammar[(Int,Int),Int] = (term(const(1)) ++ term(const(2))) <|> v
+      val v : Grammar[Unit,Int,Int] = term(1) <|> v
+      val v2 : Grammar[(Unit,Unit),(Int,Int),Int] = (term(1) ++ term(2)) <|> v2
     }
     compile(vv.v).show
   }
