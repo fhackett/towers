a44872355937dd7b77b9e84ce56a49f23b5b2c2d
fhackett <fhackett.py@gmail.com>
2019-09-26 04:34:07 -0400

    WIP explore object with apply style

--- src/main/scala/Computes.scala
+++ src/main/scala/Computes.scala
@@ -29,50 +29,86 @@ sealed abstract class Computes[T] {
   def key(given keyCtx : KeyContext) : ComputesKey = keyCtx.getKeyOf(this)
 }
 
-sealed abstract class Rule[T] {
-  type Members <: Tuple
-  type ComputesMembers <: Tuple
-  def make(members : ComputesMembers) : Computes[T]
-}
+sealed abstract class Rule {
 
-object Rule {
-  
-  abstract class R[T] {
-    
+  protected inline def rule[Handles <: Tuple, F, T : Type]
+                 (body : F)
+                 (given tpl : TupledFunction[F,Handles=>Rule.R[T]])
+                 (given Tuple.IsMappedBy[CHandle][Handles]) <: Rule.Instance[T] = {
+    val thisRule = this
+    new Rule.Instance[T] {
+      val rule = thisRule
+      type CHandles = Handles
+      type Members = Tuple.InverseMap[Handles,CHandle]
+      type ComputesMembers = Tuple.Map[Members,Computes]
+      def apply(members : ComputesMembers) : Computes[T] = {
+        lazy val handles : Handles = Rule.makeHandles[0,Handles](result)
+        lazy val ruleR : Rule.R[T] = tpl.tupled(body)(handles)
+        lazy val result : Computes[T] = ComputesComposite(
+          this, members.toArray.toList.asInstanceOf[List[Computes[_]]],
+          handles.toArray.toList.asInstanceOf[List[CHandle[_]]])
+        result
+      }
+      def matchRewrite(fn : Handles=>Rule.CRewrite[T]) : Rule.CRewrite[T] =
+        Rule.MatchRewrite(thisRule, fn)
+    }
   }
 }
 
-object Computes { 
+object Rule {
 
-  private inline def makeHandles[Index <: Int, Members <: Tuple](ownerRef : =>Computes[_]) : Members = inline erasedValue[Members] match {
+  sealed abstract class CRewrite[T] {
+    // TODO
+  }
+
+  final case class MatchRewrite[T, R <: Rule, Handles <: Tuple](val rule : R, val fn : Handles=>CRewrite[T]) extends CRewrite[T]
+
+  sealed abstract class Instance[T] {
+    val rule : Rule
+    type CHandles <: Tuple
+    type Members <: Tuple
+    type ComputesMembers <: Tuple
+    def apply(members : ComputesMembers) : Computes[T]
+    def matchRewrite(fn : CHandles=>CRewrite[T]) : CRewrite[T]
+  }
+  
+  abstract class R[T] {
+    def rewrite : Any//CRewrite[T]
+    def lower : Any//CLower[T]
+  }
+
+  private[computes] inline def makeHandles[Index <: Int, Members <: Tuple](ownerRef : =>Computes[_]) : Members = inline erasedValue[Members] match {
     case _ : Unit => ().asInstanceOf[Members]
     case _ : (CHandle[hd] *: tl) => (new CHandle[hd] {
       val index = constValue[Index]
       lazy val owner = ownerRef
     } *: makeHandles[S[Index], tl](ownerRef)).asInstanceOf[Members]
   }
-
-  inline def rule[Handles <: Tuple, F, T](body : F)(given tpl : TupledFunction[F,Handles=>Rule.R[T]])(given Tuple.IsMappedBy[CHandle][Handles]) <: Rule[T] =
-    new Rule[T] {
-      type Members = Tuple.InverseMap[Handles,CHandle]
-      type ComputesMembers = Tuple.Map[Members,Computes]
-      def make(members : ComputesMembers) : Computes[T] = {
-        lazy val ruleR : Rule.R[T] = tpl.tupled(body)(makeHandles[0,Handles](result))
-        lazy val result : Computes[T] = ComputesComposite(ruleR, members.toArray.toList.asInstanceOf[List[Computes[_]]])
-        result
-      }
-    }
 }
 
-final case class ComputesComposite[T, R <: Rule.R[T]](val rule : R, val members : List[Computes[_]]) extends Computes[T]
+object Computes {
+  def ref[T](body : (given QuoteContext)=>Computes[T]) : Computes[T] =
+    ComputesRef(body)
+}
 
-final class ComputesName[T] extends Computes[T]
+final case class ComputesComposite[T : Type, R <: Rule.Instance[T]](val rule : R, val members : List[Computes[_]], val handles : List[CHandle[_]]) extends Computes[T] {
+  val theType = implicitly[Type[T]]
+}
+
+final class ComputesName[T : Type] extends Computes[T] {
+  //val theType = the[Type[T]]
+}
+
+final class ComputesRef[T](val ref : (given QuoteContext)=>Computes[T]) extends Computes[T]
 
 // test
 
-def TupleConcat[L <: Tuple, R <: Tuple] = Computes.rule {
-  (lhs : CHandle[L], rhs : CHandle[R]) => new Rule.R[Tuple.Concat[L,R]] {
-    
+object TupleConcat extends Rule {
+  def apply[L <: Tuple : Type, R <: Tuple : Type](given QuoteContext) = rule {
+    (lhs : CHandle[L], rhs : CHandle[R]) => new Rule.R[Tuple.Concat[L,R]] {
+      def rewrite = ???
+      def lower = ???
+    }
   }
 }
 
