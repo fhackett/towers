42e766cdc3d5db45a31df483f89f5fd9d3e3dc06
fhackett <fhackett.py@gmail.com>
2019-09-26 20:54:08 -0400

    WIP add let, expr and fun primitives

--- build.sbt
+++ build.sbt
@@ -7,6 +7,6 @@ lazy val root = project
     name := "towers",
     version := "0.1.0",
 
-    scalaVersion := /*"0.17.0-RC1"*/ latestDotty,
+    scalaVersion := /*"0.19.0-RC1",*/ latestDotty,
 )
 
--- src/main/scala/Computes.scala
+++ src/main/scala/Computes.scala
@@ -20,6 +20,8 @@ trait KeyContext {
   def getKeyOf(obj : AnyRef) : ComputesKey
 }
 
+class ==>[Args <: Tuple, Result](val pc : Int, val closure : Array[Any])
+
 sealed abstract class CHandle[T] {
   val index : Int
   lazy val owner : Computes[_]
@@ -29,12 +31,79 @@ sealed abstract class Computes[T] {
   def key(given keyCtx : KeyContext) : ComputesKey = keyCtx.getKeyOf(this)
 }
 
+object Computes {
+  def ref[T](body : (given QuoteContext)=>Computes[T]) : Computes[T] = Ref(body)
+
+  def let[V:Type,T](value : Computes[V], body : Computes[V]=>Computes[T]) = {
+    val name = Name[V]()
+    Binding(name, value, body(name))
+  }
+
+  inline def expr[Args <: Tuple, Result : Type, F](args : Args, fn : F)
+                 (given tpl : TupledFunction[F,Tuple.Map[Tuple.InverseMap[Args,Computes],Expr]=>Expr[Result]])
+                 (given Tuple.IsMappedBy[Computes][Args]): Computes[Result] = {
+    type PlainArgs = Tuple.InverseMap[Args,Computes]
+    type ExprArgs = Tuple.Map[PlainArgs,Expr]
+    Code(
+      listArgs =>
+        tpl.tupled(fn)(Tuple.fromArray(listArgs.toArray).asInstanceOf[ExprArgs]),
+      args.toArray.toList.asInstanceOf[List[Computes[_]]])
+  }
+
+  inline def makeFunctionArgs[Args <: Tuple] <: Tuple = inline erasedValue[Args] match {
+    case _ : Unit => ()
+    case _ : (hd *: tl) => {
+      type H = hd
+      summonFrom {
+        case given _ : Type[H] =>
+          Name[H]() *: makeFunctionArgs[tl]
+      }
+    }
+  }
+
+  inline def fun[Args <: Tuple, Result, F](fn : F)(given tpl : TupledFunction[F,Args=>Computes[Result]])(given Tuple.IsMappedBy[Computes][Args]) : Computes[Tuple.InverseMap[Args,Computes]==>Result] = {
+    type PlainArgs = Tuple.InverseMap[Args,Computes]
+    val args : Args = makeFunctionArgs[PlainArgs].asInstanceOf[Args]
+    Function(tpl.tupled(fn)(args), args.toArray.toList.asInstanceOf[List[Name[_]]])
+  }
+
+  /*
+  inline given makeFunction[Args <: Tuple, Result, F](given tpl : TupledFunction[F,Args=>Computes[Result]])(given Tuple.IsMappedBy[Computes][Args]): Conversion[F,Computes[Tuple.InverseMap[Args,Computes]==>Result]] = new {
+    type PlainArgs = Tuple.InverseMap[Args,Computes]
+    def apply(fn : F) : Computes[PlainArgs==>Result] = {
+      val args : Args = makeFunctionArgs[PlainArgs].asInstanceOf[Args]
+      Function[PlainArgs,Result](tpl.tupled(fn)(args), args.toArray.toList.asInstanceOf[List[Name[_]]])
+    }
+  }*/
+
+  final case class Composite[T : Type, R <: Rule.Instance[T]](val rule : R, val members : List[Computes[_]], val handles : List[CHandle[_]]) extends Computes[T] {
+    val theType = summon[Type[T]]
+  }
+  
+  final case class Code[Result : Type](val fn : List[Expr[_]]=>Expr[Result], val args : List[Computes[_]]) extends Computes[Result] {
+    val theType = summon[Type[Result]]
+  }
+
+  final case class Application[Args <: Tuple, Result](val fn : Computes[Args==>Result], val args : List[Computes[_]]) extends Computes[Result]
+
+  final case class Function[Args <: Tuple, Result](val body : Computes[Result], val args : List[Name[_]]) extends Computes[Args==>Result]
+
+  final case class Binding[V, T](val name : Name[V], val value : Computes[V], val body : Computes[T]) extends Computes[T]
+
+  final class Name[T : Type] extends Computes[T] {
+    val theType = summon[Type[T]]
+  }
+
+  final class Ref[T](val ref : (given QuoteContext)=>Computes[T]) extends Computes[T]
+
+}
+
 sealed abstract class Rule {
 
   protected inline def rule[Handles <: Tuple, F, T : Type]
-                 (body : F)
-                 (given tpl : TupledFunction[F,Handles=>Rule.R[T]])
-                 (given Tuple.IsMappedBy[CHandle][Handles]) <: Rule.Instance[T] = {
+                           (body : F)
+                           (given tpl : TupledFunction[F,Handles=>Rule.R[T]])
+                           (given Tuple.IsMappedBy[CHandle][Handles]) <: Rule.Instance[T] = {
     val thisRule = this
     new Rule.Instance[T] {
       val rule = thisRule
@@ -44,7 +113,7 @@ sealed abstract class Rule {
       def apply(members : ComputesMembers) : Computes[T] = {
         lazy val handles : Handles = Rule.makeHandles[0,Handles](result)
         lazy val ruleR : Rule.R[T] = tpl.tupled(body)(handles)
-        lazy val result : Computes[T] = ComputesComposite(
+        lazy val result : Computes[T] = Computes.Composite(
           this, members.toArray.toList.asInstanceOf[List[Computes[_]]],
           handles.toArray.toList.asInstanceOf[List[CHandle[_]]])
         result
@@ -85,21 +154,6 @@ object Rule {
     } *: makeHandles[S[Index], tl](ownerRef)).asInstanceOf[Members]
   }
 }
-
-object Computes {
-  def ref[T](body : (given QuoteContext)=>Computes[T]) : Computes[T] =
-    ComputesRef(body)
-}
-
-final case class ComputesComposite[T : Type, R <: Rule.Instance[T]](val rule : R, val members : List[Computes[_]], val handles : List[CHandle[_]]) extends Computes[T] {
-  val theType = implicitly[Type[T]]
-}
-
-final class ComputesName[T : Type] extends Computes[T] {
-  //val theType = the[Type[T]]
-}
-
-final class ComputesRef[T](val ref : (given QuoteContext)=>Computes[T]) extends Computes[T]
 
 // test
 
